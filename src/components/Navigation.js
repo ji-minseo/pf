df0228cc293d7507c609d56937f0350d3dce7116
ji-minseo <you@example.com>
2020-12-02 09:05:29 +0900

    add router

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 function Navigation() {
     return <div>
-        <Link to="/">Lany</Link>
+        <Link to="/"><p className="ff-castoro fs-small fw-light">Lany</p></Link>
+
         <Link to="/about">About</Link>
     </div>
 }
