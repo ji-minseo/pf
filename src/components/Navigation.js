8bf05ad314dea6135f14a4231db25770616c2e4a
ji-minseo <you@example.com>
2020-12-02 13:39:03 +0900

    add activeclassname

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navigation() {
     return <div>
-        <Link to="/lany"><p className="ff-castoro fs-small fw-light">lany businesscard</p></Link>
-        <Link to="/axis"><p className="ff-castoro fs-small fw-light">axis of horizon</p></Link>
-        <Link to="/jacq"><p className="ff-castoro fs-small fw-light">jacquemus</p></Link>
-        <Link to="/gust"><p className="ff-castoro fs-small fw-light">gustafwestman</p></Link>
+        <NavLink to="/lany" activeClassName="selected"><p className="ff-castoro fs-small fw-light">lany businesscard</p></NavLink>
+        <NavLink to="/axis" activeClassName="selected"><p className="ff-castoro fs-small fw-light">axis of horizon</p></NavLink>
+        <NavLink to="/jacq" activeClassName="selected"><p className="ff-castoro fs-small fw-light">jacquemus</p></NavLink>
+        <NavLink to="/gust" activeClassName="selected"><p className="ff-castoro fs-small fw-light">gustafwestman</p></NavLink>
 
     </div>
 }
