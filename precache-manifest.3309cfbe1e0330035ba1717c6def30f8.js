e915e4bc5dc054539dcc2704843143a41a986d51
ji-minseo <you@example.com>
2020-12-15 19:52:43 +0900

    Updates

---
+++ precache-manifest.3309cfbe1e0330035ba1717c6def30f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f7ca45d449be6443030dff0e322353",
+    "url": "/pf/index.html"
+  },
+  {
+    "revision": "fe5beef4397ae370fe8b",
+    "url": "/pf/static/css/main.a69caada.chunk.css"
+  },
+  {
+    "revision": "bd939291a1df2002a231",
+    "url": "/pf/static/js/2.f53827ee.chunk.js"
+  },
+  {
+    "revision": "0892a97823e625ab5aa44de2db0b479e",
+    "url": "/pf/static/js/2.f53827ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5beef4397ae370fe8b",
+    "url": "/pf/static/js/main.a0134916.chunk.js"
+  },
+  {
+    "revision": "ebdd9901588e9aeb2175",
+    "url": "/pf/static/js/runtime-main.4342bbc8.js"
+  }
+]);
