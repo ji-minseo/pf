bd8d2276524c740fe2417622c1444b8f70b84d24
서지민 <seojimin@seojimicBookAir.lan>
2021-02-09 23:52:39 +0900

    Updates

---
+++ precache-manifest.dc951634eb24ce7e1aa6820497705870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459a9e86f3c1addba25b18c6712fb92d",
+    "url": "/pf/index.html"
+  },
+  {
+    "revision": "69a7d08641fd9943bae5",
+    "url": "/pf/static/css/main.aec5552b.chunk.css"
+  },
+  {
+    "revision": "8fefc6f6ce1ac66b0c76",
+    "url": "/pf/static/js/2.a14ccf70.chunk.js"
+  },
+  {
+    "revision": "0892a97823e625ab5aa44de2db0b479e",
+    "url": "/pf/static/js/2.a14ccf70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a7d08641fd9943bae5",
+    "url": "/pf/static/js/main.e19089f1.chunk.js"
+  },
+  {
+    "revision": "ebdd9901588e9aeb2175",
+    "url": "/pf/static/js/runtime-main.4342bbc8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pf/precache-manifest.d18253c9946ba2627e8c962dfca43734.js"
+  "/pf/precache-manifest.dc951634eb24ce7e1aa6820497705870.js"
 );
 
 self.addEventListener('message', (event) => {
