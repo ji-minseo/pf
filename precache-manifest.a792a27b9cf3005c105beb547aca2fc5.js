df8bba548d454ae4ce74f4b4f874a0323f8217b6
서지민 <seojimin@seojimicBookAir.lan>
2021-02-11 22:41:46 +0900

    Updates

---
+++ precache-manifest.a792a27b9cf3005c105beb547aca2fc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b394c195060c410d9f23f4082f2b2cd7",
+    "url": "/pf/index.html"
+  },
+  {
+    "revision": "9c18740eca9f663d6536",
+    "url": "/pf/static/css/main.aec5552b.chunk.css"
+  },
+  {
+    "revision": "8fefc6f6ce1ac66b0c76",
+    "url": "/pf/static/js/2.a14ccf70.chunk.js"
+  },
+  {
+    "revision": "0892a97823e625ab5aa44de2db0b479e",
+    "url": "/pf/static/js/2.a14ccf70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c18740eca9f663d6536",
+    "url": "/pf/static/js/main.032d3c08.chunk.js"
+  },
+  {
+    "revision": "ebdd9901588e9aeb2175",
+    "url": "/pf/static/js/runtime-main.4342bbc8.js"
+  }
+]);
